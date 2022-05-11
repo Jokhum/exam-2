7f5e780798472360653f4ee90f11271df46aa373
Jokhum <p.werven@gmail.com>
2022-05-11 19:16:51 +0200

    dsiable line

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -28,7 +28,7 @@ export default function LoginForm() {
     resolver: yupResolver(schema),
   });
 
-  const [auth, setAuth] = useContext(AuthContext);
+  const [auth, setAuth] = useContext(AuthContext); //eslint-disable-line
 
   async function onSubmit(data) {
     setSubmitting(true);
